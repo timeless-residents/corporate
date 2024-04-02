4072d47d4717480aa782f75767b0974e7d87754d
Takuya Sato <business@satotakuya.jp>
2024-04-02 22:36:37 +0900

    fix layout

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
 
 function toggleNav() {
+  document.body.classList.toggle('nav-active');
   nav.classList.toggle('nav-active');
-
   navLinks.forEach((link, index) => {
     if (link.style.animation) {
       link.style.animation = '';
@@ -13,24 +13,36 @@ function toggleNav() {
       link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
     }
   });
-
   burger.classList.toggle('toggle');
 }
 
 function closeNav() {
+  document.body.classList.remove('nav-active');
   nav.classList.remove('nav-active');
-
   navLinks.forEach((link) => {
     link.style.animation = '';
   });
-
   burger.classList.remove('toggle');
 }
 
 burger.addEventListener('click', toggleNav);
 
 navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    
+    if (targetSection) {
+      const headerHeight = document.querySelector('header').offsetHeight;
+      const targetPosition = targetSection.offsetTop - headerHeight;
+      
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+    
     closeNav();
   });
 });
